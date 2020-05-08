a7a4bed6446327c489bc138727349612a1368b46
Vlad <vlad.gheorghita.s@outlook.com>
2020-05-08 13:48:44 +0300

    add

---
+++ WorkshopManagement/Models/Domain/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkshopManagement.Models.Domain
+{
+    class User
+    {
+        private String firstName { get; set; }
+        private String lastName { get; set; }
+        private String email { get; set; }
+    }
+}
---
+++ WorkshopManagement/Models/Repository/RepoUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkshopManagement.Models.Repository
+{
+    class RepoUser
+    {
+    }
+}
